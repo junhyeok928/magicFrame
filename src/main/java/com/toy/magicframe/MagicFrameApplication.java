7ea3442f0c698196ad4f5f5dedf5e4e39cb55ec0
junhyeok928 <junhyeok928@naver.com>
2022-06-14 21:38:42 +0900

    first commit

---
+++ src/main/java/com/toy/magicframe/MagicFrameApplication.java
@@ -0,0 +1,13 @@
+package com.toy.magicframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicFrameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicFrameApplication.class, args);
+    }
+
+}
